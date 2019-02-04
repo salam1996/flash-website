d87f363c8e01dd3e2a0fe652612fca69dddbf4db
maishaidris <maishaidris@users.noreply.github.com>
2019-02-04 11:36:28 -0500

    Update courses and extra php to remove redundancy

--- courses.php
+++ courses.php
@@ -4,7 +4,6 @@
 		<p>
 			Along with CSC 174, this semester I’m taking CSC 191/291 (Machines & Consciousness), CSC 242 (Introduction to Artificial Intelligence), MUR 168A (West African Drumming Introduction), and PSC 288 (Game Theory).  Some of my favorite courses in the past have been CSC 173 (Computation & Formal Systems), PSC 248 (Discrimination), CSC 170 (Introduction to Web Development), CSC 299W (Social Implications of Computing), and all of the courses required for my cluster in American Sign Language.  I wish I could go back in time, and try out some new courses, or audit more courses I'm interested in but that don't fit my degree requirements.  Some of my favorite courses, such as CSC 170 and PSC 248, started off as interests or curiosity that did not contribute to my degree (not my major, minor, or cluster), but the course either eventually became applicable once I realized it was in line with what I wanted to do or pointed me towards more courses in topics I discovered that I enjoyed.
 		</p>
-	</main>
 	<?php include "inc/sharon.php"; ?>
 
 			<p>
